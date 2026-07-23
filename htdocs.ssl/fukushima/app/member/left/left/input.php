510b2087341cf2dc5a50fbed19ba4c4ad19934b9
SHIROTA Kiyohiro <shirota.kiyohiro@u-coop.jp>
2026-07-23 10:18:38 +0900

    既卒者向け出資金返還フォーム

---
+++ htdocs.ssl/fukushima/app/member/left/left/input.php
@@ -0,0 +1,34 @@
+<?php
+
+$pp = 'edit_';
+while (list($key, $value) = each($method)) {
+	if (!preg_match('/^extra/', $key)) {
+		$html .= $smarty->fetch($pp . $key . '.tpl');
+	} else {
+		$k = intval(substr($key, 5));
+		$smarty->assign('k', $k);
+		$extraList[$k] = [];
+		if ($category['method']['extra'][$k]['select']) {
+			$select = trim($category['method']['extra'][$k]['select']);
+			$select = preg_replace('/\n|\r\n/', "\n", $select);
+
+			$extraList[$k] = explode("\n", $select);
+			if ($k == 0) {
+				foreach ($extraList[$k] as $v) {
+					$tmp = explode(",", $v);
+					$tmps[$tmp[0]] = $tmp[1];
+				}
+				$extraList[$k] = array_keys($tmps);
+			}
+
+		}
+		$smarty->assign('extraList', $extraList[$k]);
+
+		$html .= $smarty->fetch($pp . 'extra.tpl');
+	}
+}
+
+$smarty->assign("html", $html); //項目のテンプレート発行
+
+$smarty->display('input.tpl');
+?>
